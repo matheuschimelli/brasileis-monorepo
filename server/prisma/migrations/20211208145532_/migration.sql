cb5f077b255cf39f955e6d76765c93b934bd081e
Matheus Chimelli <matheuschimelli7@gmail.com>
2021-12-09 01:23:23 -0300

    wip law parser class

---
+++ server/prisma/migrations/20211208145532_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "LawBlock" ADD COLUMN     "name" TEXT;
--- server/src/utils/LawParserClass.js
+++ server/src/utils/LawParserClass.js
@@ -23,16 +23,22 @@ class LawParser {
 
         this.lastInsertType = null
 
+        this.parentType = null
+
         this.insertHistory = []
 
         this.rawParagraphs = []
         this.articles = []
         this.temporaryContent = []
 
+        this.orderedArray = []
+
         this.matchArticlesRegex = /^(^(Art|art|Artigo|artigo). ([+-]?([0-9]+\.?[0-9]*|\.[0-9]+)).*$)/;
         this.matchArticleNumber = /^((^(Art|art|Artigo|artigo). ([+-]?([0-9]+\.?[0-9]*|\.[0-9]+))(-?[A-Z]?))|(^(Art|art|Artigo|artigo) ([+-]?([0-9]+\.?[0-9]*|\.[0-9]+))(-?[A-Z]?)))/;
         this.matchRomanNumber = /^(^(^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})) (-))/;
         this.matchParagraph = /^((§) ([+-]?([0-9]+\.?[0-9]*|\.[0-9]+)))/;
+        this.matchAlinea = /^(([a-zA-Z])(\)) .*(;))/;
+        this.matchAlineaLetter = /^(([a-zA-Z])(\)))/;
         this.matchParagrafoUnico = /^(Parágrafo único)/;
         this.paragraphEndStartIncise = /^(§ ([0-9]+).*(:))/;
         this.matchDot = /(\.)$/;
@@ -40,10 +46,13 @@ class LawParser {
         this.paragraphSymbol = /(§)/;
         this.matchArticleNumberBeginning = /^(^([+-]?([0-9]+\.?[0-9]*|\.[0-9]+))(-?[A-Z]?))/;
 
+        this.paragraphAlineaRegex = /^((^(^(^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})) (-))).*(:))/;
+
         this.lastInsertArticle = true
         this.lastInsertIncise = false
         this.lastInsertParagraph = false
         this.willBeIncise = false;
+        this.willBeAlinea = false
     }
 
     clearTempArticle() {
@@ -103,6 +112,20 @@ class LawParser {
         return false
     }
 
+    hasAlineasOnIncise(str) {
+        if (str.match(this.paragraphAlineaRegex) && str.match(this.paragraphAlineaRegex)[0]) {
+            return true
+        }
+        return false
+    }
+
+    isAlinea(str) {
+        if (str.match(this.matchAlinea) && str.match(this.matchAlinea)[0]) {
+            return true
+        }
+        return false
+    }
+
     isIncise(str) {
         if (str.match(this.matchRomanNumber) && str.match(this.matchRomanNumber)[0]) {
             return true
@@ -130,6 +153,12 @@ class LawParser {
         }
         return null
     }
+    getAlineaLetter(str) {
+        if (str.match(this.matchAlineaLetter)) {
+            return str.match(this.matchAlineaLetter)[0].replace(")", "").trim()
+        }
+        return null
+    }
 
     getText(str) {
         return str.replace("Art. ", "").replace("art. ", "").replace(new RegExp(this.matchArticleNumberBeginning), "").replace(new RegExp(this.matchOStart), "").replace("Parágrafo Único. ", "").replace(new RegExp(this.matchParagrafoUnico), "").replace(new RegExp(this.matchRomanNumber), "").replace(new RegExp(this.matchParagraph), "").replace("°", "").replace("º", "").trim()
@@ -139,6 +168,7 @@ class LawParser {
         const articleNumber = this.getArticleNumber(str)
         const inciseNumber = this.getInciseNumber(str)
         const lawParagraphNumber = this.getLawParagraphNumber(str)
+        const alineaLetter = this.getAlineaLetter(str)
 
         if (articleNumber) {
             return articleNumber
@@ -149,7 +179,22 @@ class LawParser {
         if (lawParagraphNumber) {
             return lawParagraphNumber
         }
+        if (alineaLetter) {
+            return alineaLetter
+        }
+    }
 
+    getParagraphType(paragraph) {
+        if (this.isLawArticle(paragraph))
+            return 'ARTIGO_LEI'
+        if (this.isLawParagrph(paragraph))
+            return 'PARAGRAFO_LEI'
+        if (this.isLawParagrafoUnico(paragraph))
+            return 'PARAGRAFO_UNICO_LEI'
+        if (this.isIncise(paragraph))
+            return 'INCISO_LEI'
+        if (this.isAlinea(paragraph))
+            return 'ALINEA_LEI'
     }
 
     handleType(lawType) {
@@ -178,6 +223,7 @@ class LawParser {
             identifier: `${this.slug} art.${this.currentArticleNumber} ${this.handleType(type)} ${this.getNumber(paragraph)}`,
 
             title: `Código de Defesa do Consumidor ${this.handleType(type)} ${this.getNumber(paragraph)}`,
+            name: '',
             value: this.getText(paragraph),
             source: window.location.href,
             version: 1,
@@ -204,7 +250,7 @@ class LawParser {
         const history = this.insertHistory.reverse()
         var found = null
 
-        if (currentType === history[0]) {
+        if (currentType == history[0]) {
             for (const item of history) {
                 if (item !== currentType) {
                     found = item
@@ -212,9 +258,7 @@ class LawParser {
                 }
             }
         }
-
         return (found !== '' && found !== null && found !== undefined) ? found : currentType
-
     }
 
     processSingleParagraph(paragraph) {
@@ -233,63 +277,62 @@ class LawParser {
             this.tempParagraphs = []
             this.temporaryContent = []
 
+            this.parentType = 'ARTIGO_LEI'
+
             var tempAr = this.generateArticle({
                 paragraph,
                 type: 'ARTIGO_LEI',
                 content: this.temporaryContent,
                 parentType: null,
             })
-
-            this.insertHistory.push('ARTIGO_LEI')
-            this.lastInsertType = this.getLastInsertType('ARTIGO_LEI')
-
             this.currentArticleNumber = this.getNumber(paragraph)
-
             this.tempArticle = tempAr
-
         }
 
         // Verifica se é Parágrafo Único de Lei
         if (this.isLawParagrafoUnico(paragraph)) {
+
             this.lastInsertArticle = false
             this.willBeInsice = false
-
-            this.insertHistory.push('PARAGRAFO_UNICO_LEI')
-            this.lastInsertType = this.getLastInsertType('PARAGRAFO_UNICO_LEI')
+            this.parentType = 'ARTIGO_LEI'
 
             this.temporaryContent.push(this.generateArticle({
                 number: this.currentArticleNumber,
                 paragraph: paragraph,
                 content: [],
                 type: 'PARAGRAFO_UNICO_LEI',
-                parentType: this.lastInsertType,
-
+                parentType: this.parentType,
             }))
-
         }
 
         // Verifica se é um Parágrafo normal de artigo de Lei
         if (this.isLawParagrph(paragraph)) {
+
             this.lastInsertArticle = false
             this.willBeInsice = false
-
-            this.insertHistory.push('PARAGRAFO_LEI')
-            this.lastInsertType = this.getLastInsertType('PARAGRAFO_LEI')
 
             this.temporaryContent.push(this.generateArticle({
                 number: this.currentArticleNumber,
                 paragraph: paragraph,
                 content: [],
                 type: 'PARAGRAFO_LEI',
-                parentType: this.lastInsertType,
-
+                parentType: this.parentType,
             }))
-
+            this.parentType = 'PARAGRAFO_LEI'
         }
 
         if (this.isParagraphIncise(paragraph)) {
+            this.parentType = 'PARAGRAFO_LEI'
             this.lastInsertArticle = false
             this.willBeInsice = true
+            this.willBeAlinea = false
+        }
+
+        if (this.hasAlineasOnIncise(paragraph)) {
+            this.parentType = 'ALINEA_LEI'
+            this.lastInsertArticle = false
+            this.willBeInsice = false
+            this.willBeAlinea = true
         }
 
         // Verifica se é um inciso de artigo de lei
@@ -301,33 +344,73 @@ class LawParser {
                 var lastContent = this.temporaryContent.slice(-1)[0]
 
                 if (lastContent && lastContent.content) {
-                    this.insertHistory.push('INCISO')
-                    this.lastInsertType = this.getLastInsertType('INCISO')
+
                     lastContent.content.push(this.generateArticle({
                         number: this.currentArticleNumber,
                         paragraph: paragraph,
                         content: [],
                         type: 'INCISO',
-                        parentType: this.lastInsertType,
-
+                        parentType: this.parentType,
                     }))
-
                 }
 
             } else {
-                this.insertHistory.push('INCISO')
-                this.lastInsertType = this.getLastInsertType('INCISO')
                 // Verifica se é um inciso comum de artigo de lei
                 this.temporaryContent.push(this.generateArticle({
                     number: this.currentArticleNumber,
                     paragraph: paragraph,
                     content: [],
                     type: 'INCISO',
-                    parentType: this.lastInsertType,
-
+                    parentType: this.parentType,
                 }))
-
+                this.parentType = 'INCISO'
             }
+        }
+
+        /*
+
+        // veririca se vai ser alinea
+        if (this.hasAlineasOnIncise(paragraph)) {
+
+            if (this.willBeAlinea) {
+                var lastContent = this.temporaryContent.slice(-1)[0]
+
+                if (lastContent && lastContent.content) {
+
+                    lastContent.content.push(this.generateArticle({
+                        number: this.currentArticleNumber,
+                        paragraph: paragraph,
+                        content: [],
+                        type: 'ALINEA_LEI',
+                        parentType: this.parentType,
+                    }))
+                }
+            } else {
+                // Verifica se é uma alinea  comum de artigo de lei
+                this.temporaryContent.push(this.generateArticle({
+                    number: this.currentArticleNumber,
+                    paragraph: paragraph,
+                    content: [],
+                    type: 'ALINEA_LEI',
+                    parentType: this.parentType,
+                }))
+                this.parentType = 'INCISO'
+            }
+        }
+        */
+    }
+
+    orderedProcessor(paragraph) {
+        paragraph = paragraph.innerText.trim()
+
+        if (paragraph !== '') {
+            this.orderedArray.push({
+                type: this.getParagraphType(paragraph),
+                name: this.getNumber(paragraph),
+                value: this.getText(paragraph),
+                originalText: paragraph
+
+            })
         }
     }
 
@@ -337,13 +420,20 @@ class LawParser {
         }
     }
 
+    processParagraphsOrderedList() {
+        if (this.rawParagraphs.length !== 0) {
+            this.rawParagraphs.forEach(el => this.orderedProcessor(el))
+        }
+    }
+
     start() {
         this.removeAnchors()
         this.getParagraphs("body")
         this.processParagraphs()
+        this.processParagraphsOrderedList()
         this.articles = this.articles.filter(value => Object.keys(value).length !== 0)
         console.log(this.articles)
-        console.log(this.insertHistory)
+        console.log(this.orderedArray)
     }
 }
 
@@ -356,12 +446,32 @@ function iterate(arr) {
     for (const item of arr) {
         if (item.content && item.content.length !== 0) {
             tempArray.push(item)
-            console.log(item)
+
+            console.log('iterating', item)
+
             iterate(item.content)
         } else {
             tempArray.push(item)
 
-            console.log(item)
+            console.log('iterating', item)
         }
     }
+}
+
+/*
+- pega a lista de items e o tipo
+- inverte a lista para que o ultimo item seja o primeiro
+- se o primeiro item da lista for diferente do tipo retorna o elemento da lista
+*/
+function findPreviusItemInHistory(arr, type) {
+    var reversed = arr.reverse()
+    var found = null
+
+    for (const item of reversed) {
+        if (item != type) {
+            found = item
+            break;
+        }
+    }
+    return found
 }
