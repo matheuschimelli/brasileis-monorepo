958405d1a2889b8f0df7280fa0785868a07af1a4
Matheus Chimelli <matheuschimelli7@gmail.com>
2022-01-11 17:14:39 -0300

    closes #43

--- nextclient/components/layout/DefaultLayout.tsx
+++ nextclient/components/layout/DefaultLayout.tsx
@@ -4,6 +4,7 @@ import { NextSeo } from "next-seo";
 import Header from "./Header";
 import { Footer } from "./Footer";
 import { Stack, Box } from "@chakra-ui/react";
+import MobileNav from './MobileNav'
 
 type Props = {
   children: React.ReactNode;
@@ -23,6 +24,7 @@ export default function DefaultLayout({ children, title, searchQuery }: Props) {
       <Stack as="main" px={{ base: "0", sm: "4", md: "20" }} py={["0", "0", "0"]}>
         <Box mt={["0", "0", "0"]}>{children}</Box>
       </Stack>
+      <MobileNav />
       <Footer />
     </>
   );
